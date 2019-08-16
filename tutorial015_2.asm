da97c4b10b901f2291995166e231c71ac81d6d8c
davidseungjin <ryonlee@gmail.com>
2019-08-16 12:09:39 -0700

    mips

---
+++ tutorial015_2.asm
@@ -0,0 +1,24 @@
+# pread.asm -- prompt for and read an integer
+#
+# on entry:
+#    $ra -- return address
+#
+# on exit:
+#    $v0 -- the integer
+
+         .text
+         .globl  pread
+pread:   
+         la    $a0,prompt        # print string
+         li    $v0,4             # service 4
+         syscall
+        
+         li    $v0,5             # read int into $v0
+         syscall                 # service 5
+        
+         jr    $ra               # return
+         nop                     #  
+        
+         .data
+prompt:
+         .asciiz "Enter an integer: "
